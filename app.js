00cada5a3912614ee33c0097d00c278c29cc2857
VARSHAN69 <varshan.dev.69@gmail.com>
2026-05-31 12:55:18 +0530

    Initial commit: Apex Cash Flow Ledger template

---
+++ app.js
@@ -0,0 +1,9 @@
+
+            document.getElementById('cf-json').innerHTML = JSON.stringify({
+                "Operating Inflows": "$8,500.00",
+                "Marketing Outflows": "-$2,200.00",
+                "Rent & Utilities": "-$1,850.00",
+                "Software Licensing": "-$2,000.00",
+                "Net Monthly Margin": "+$2,450.00"
+            }, null, 2);
+        
